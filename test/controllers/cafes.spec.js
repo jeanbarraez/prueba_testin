203d2321b8efefa6232b226e66efed3034f4158c
jean barraez <jeanbarraez@users.noreply.github.com>
2024-02-03 13:08:02 -0300

    se modifico el test controllers, cafes.spec.js

--- test/controllers/cafes.spec.js
+++ test/controllers/cafes.spec.js
@@ -6,13 +6,11 @@ import { faker } from "@faker-js/faker";
 
 describe("Operaciones CRUD de cafes", () => {
   it("should return a status code 200 and an array with at least one object", async () => {
-    const response = await request(app).get("/cafes").send(); //utiliza la función request de Supertest para realizar una solicitud GET a la ruta /cafes
+    const response = await request(app).get("/cafes").send();
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true); //verifica si el valor proporcionado es un arreglo. Si el valor es un arreglo,
-    //la función devuelve true; de lo contrario, devuelve false
-    expect(response.body.length).toBeGreaterThanOrEqual(1); //toBeGreaterThanOrEqual compara lo recido >= esperado para valores numericos y que tenga 1 objeto
-    //la prueba está verificando que la respuesta de la solicitud a la ruta /cafes contenga al menos un objeto en forma de arreglo.
-    //Si la longitud del arreglo en la respuesta es 1 o más, la prueba pasará; de lo contrario, fallará
+    expect(Array.isArray(response.body)).toBe(true);
+
+    expect(response.body.length).toBeGreaterThanOrEqual(1);
   });
   describe("DELETE /cafes/:id", () => {
     it("should return a 404 status when trying to update a non-existent product", async () => {
@@ -61,7 +59,7 @@ describe("Operaciones CRUD de cafes", () => {
   describe("PUT /cafes", () => {
     it("should return a 400 status when trying to update a non-existent product", async () => {
       const nonExistentId = faker.string.uuid();
-       // Genera un ID único con Faker
+      // Genera un ID único con Faker
       const updatedProduct = {
         id: nonExistentId, // Utiliza el ID no existente
         name: "espresso",
